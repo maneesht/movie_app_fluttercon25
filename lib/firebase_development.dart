e14f806fbc9c8613f6a74a219e1d240010ef9014
Maneesh Tewani <mtewani@google.com>
2025-06-23 21:12:28 -0700

    Added FDC as a dep

--- lib/firebase_development.dart
+++ lib/firebase_development.dart
@@ -2,8 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:movie_app_fluttercon25/config/dependencies.dart';
 import 'package:movie_app_fluttercon25/main.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_data_connect/firebase_data_connect.dart';
 
 void main() {
+  FirebaseDataConnect fdc = FirebaseDataConnect.instanceFor(
+      connectorConfig:
+          ConnectorConfig("us-central1", "default", "movieappfluttercon25"));
+  fdc.useDataConnectEmulator('localhost', 9399);
   runApp(MultiProvider(
     providers: providersFirebase,
     child: const MainApp(),
