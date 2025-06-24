1088a3e71879e97eca673e66641b987fe3364bf1
Maneesh Tewani <mtewani@google.com>
2025-06-24 16:58:52 -0700

    Updated snippets and emulator

--- lib/firebase_development.dart
+++ lib/firebase_development.dart
@@ -7,8 +7,8 @@ import 'package:firebase_data_connect/firebase_data_connect.dart';
 void main() {
   FirebaseDataConnect fdc = FirebaseDataConnect.instanceFor(
       connectorConfig:
-          ConnectorConfig("us-central1", "default", "movieappfluttercon25"));
-  fdc.useDataConnectEmulator('localhost', 9399);
+          ConnectorConfig("us-central1", "movies", "movieappfluttercon25"));
+  fdc.useDataConnectEmulator('10.0.2.2', 9399);
   runApp(MultiProvider(
     providers: providersFirebase,
     child: const MainApp(),
--- seed-data.ts
+++ seed-data.ts
@@ -2,6 +2,7 @@ import { initializeApp } from 'firebase-admin/app';
 import { getDataConnect } from 'firebase-admin/data-connect';
 import * as movieJson from './lib/static/movies.json';
 
+process.env.DATA_CONNECT_EMULATOR_HOST="127.0.0.1:9399";
 initializeApp();
 const dataConnect = getDataConnect({ location: 'us-central1', serviceId: 'movieappfluttercon25' });
 
