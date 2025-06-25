54c01792f22aa0cae2ba78fcb85622adb940e2fb
Maneesh Tewani <mtewani@google.com>
2025-06-25 09:13:05 -0400

    Started out with better values

--- lib/services/firebase/firebase_data_service.dart
+++ lib/services/firebase/firebase_data_service.dart
@@ -2,35 +2,35 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:movie_app_fluttercon25/model/movie.dart';
 
 class FirebaseDataService {
-  Future<List<Movie>> getMostRecentMovies() async {
+  Future<void> addUser(User user) async {
     throw UnimplementedError();
   }
 
-  Future<Movie> getMovie(String id) async {
-    throw UnimplementedError();
+  Future<List<Movie>> getMostRecentMovies() async {
+    return [];
   }
 
   Future<List<Movie>> getTopTenMovies() async {
-    throw UnimplementedError();
+    return [];
   }
 
   Future<List<Movie>> getAllMovies() async {
-    throw UnimplementedError();
+    return [];
   }
 
   Future<bool> addWatched(String id) async {
-    throw UnimplementedError();
+    return true;
   }
 
   Future<bool> removeWatched(String id) async {
-    throw UnimplementedError();
+    return true;
   }
 
   Future<bool> isWatched(String id) async {
-    throw UnimplementedError();
+    return false;
   }
 
-  Future<void> addUser(User user) async {
+  Future<Movie> getMovie(String id) async {
     throw UnimplementedError();
   }
 }
